0eb954fb989ae97f3cf18a18233d9800c0d6c646
lakshyaprajapat6990 <you@example.com>
2026-02-25 00:21:04 +0530

    fixed backend url

--- DevYogam_BE-main/app.js
+++ DevYogam_BE-main/app.js
@@ -16,7 +16,7 @@ const swaggerUi = require("swagger-ui-express");
 const app = express();
 
 app.use(cors({
-  origin: "https://2-devyogam24-02-26-qmep.vercel.app/"
+  origin: "https://2-devyogam24-02-26-6mw7.vercel.app/"
 }));
 
 app.use(express.json());
